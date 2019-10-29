c685b5e3bd3b4b0f87773f29244096b73d724691
ankit.kumar_Backend <ankit.kumar@appinventiv.com>
2019-10-29 14:00:10 +0530

    update

--- users-service/src/databases/mongo.ts
+++ users-service/src/databases/mongo.ts
@@ -2,7 +2,7 @@ import { connect, set, connection as db } from 'mongoose'
 import * as config from 'config'
 import * as helper from '../utils'
 
-export class mongo {
+export class Mongo {
   private mongoUrl = config.get<string>('mongo.url')
 
   constructor() { }
@@ -10,12 +10,9 @@ export class mongo {
   async mongoConnect(server) {
     set('debug', true)
     set('useFindAndModify', false)
-    db.on('error', err => {
-      helper.consolelog('Database error. ', err, false)
-    })
-      .on('close', (error) => {
-        helper.consolelog('Database connection closed. ', error, false)
-      })
+    db.on('error', err => { helper.consolelog('Database error. ', err, false) })
+      .on('close', (err) => { helper.consolelog('Database connection closed. ', err, false) })
+
     connect(this.mongoUrl, { useCreateIndex: true, useNewUrlParser: true }, function (err) {
       if (err) {
         helper.consolelog('Database connection error. ', err, false)
@@ -27,4 +24,4 @@ export class mongo {
   }
 }
 
-export const mongoC = new mongo();
+export const mongoC = new Mongo();
