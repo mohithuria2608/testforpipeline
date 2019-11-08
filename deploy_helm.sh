5e656a934128bdfa0e27d218ea2c8673af6b714d
suruchi <suruchi.singh@appinventiv.com>
2019-11-08 15:46:12 +0530

    Update deploy_helm.sh

--- deploy_helm.sh
+++ deploy_helm.sh
@@ -5,8 +5,8 @@ echo $Users_image
 
 cat "./deployment.yaml"
 template=`cat "./deployment.yaml" | sed "s@{{Users_image}}@$Users_image@g"`
-template=`cat $template | sed "s@{{Auth_image}}@$Auth_image@g"`
-template=`cat $template | sed "s@{{Menu_image}}@$Menu_image@g"`
+template=`echo $template | sed "s@{{Auth_image}}@$Auth_image@g"`
+template=`echo $template | sed "s@{{Menu_image}}@$Menu_image@g"`
 
 # apply the yml with the substituted value
 echo $template
