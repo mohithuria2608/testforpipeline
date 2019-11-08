b4d610bf58cd1637cad063dd8fd1cc0d4e22c54a
suruchi <suruchi.singh@appinventiv.com>
2019-11-08 16:41:39 +0530

    Update deploy_helm.sh

--- deploy_helm.sh
+++ deploy_helm.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-set -e
-#az login --service-principal -u $AZ_USR -p $AZ_PASS --tenant $AZ_TENANT
-az aks get-credentials --resource-group=americana  --name=Americana-backend-cluster
+
 
 # read the yml template from a file and substitute the string 
 
