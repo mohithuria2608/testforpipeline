4d00c65b8850495f4871d98199d0ae2d95d9d46a
suruchi <suruchi.singh@appinventiv.com>
2019-11-08 15:24:23 +0530

    Update deploy_helm.sh

--- deploy_helm.sh
+++ deploy_helm.sh
@@ -4,7 +4,7 @@
 # read the yml template from a file and substitute the string 
 
 
-template=`cat "deployment.yml" | sed "s/{{Users_image}}/$Users_image/g"`
+template=`cat "${env.WORKSPACE}/deployment.yaml" | sed "s/{{Users_image}}/$Users_image/g"`
 template=`cat $template | sed "s/{{Auth_image}}/$Auth_image/g"`
 template=`cat $template | sed "s/{{Menu_image}}/$Menu_image/g"`
 
