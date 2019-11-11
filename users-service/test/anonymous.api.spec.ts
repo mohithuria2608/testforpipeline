5f7cdba1cc026e16d03f7c77b60d86304f7690cb
Ankit Nautiyal <ankit.nautiyal@appinventiv.com>
2019-11-11 13:54:11 +0530

    test case for guest login

--- users-service/test/anonymous.api.spec.ts
+++ users-service/test/anonymous.api.spec.ts
@@ -13,7 +13,7 @@ chai.use(chaiHttp);
 // const expect = chai.expect;
 
 describe("/login api where language type is English and devicetype is Android ", () => {
-  it("user is logged  ", (done) => {
+  it("Logged in Successfully", (done) => {
     const headers = {
       language: "En",
       appversion: "ACTIVE",
@@ -33,13 +33,14 @@ describe("/login api where language type is English and devicetype is Android ",
         }
 
         expect(res).to.have.status(200);
+        expect(res.body.statusCode).to.equals(200)
         done();
       });
   });
 });
 
 describe("/login api where language type is Arab and device type is android ", () => {
-  it("user is logged  ", (done) => {
+  it("Logged in Successfully", (done) => {
     const headers = {
       language: "Ar",
       appversion: "ACTIVE",
@@ -57,7 +58,6 @@ describe("/login api where language type is Arab and device type is android ", (
         if (err) {
           return "error occured '{err}'";
         }
-
         expect(res).to.have.status(200);
         done();
       });
@@ -65,7 +65,7 @@ describe("/login api where language type is Arab and device type is android ", (
 });
 
 describe("/login api where language type is En but DeviceType is wrong", () => {
-  it("user is logged  ", (done) => {
+  it("Logged in Failed", (done) => {
     const headers = {
       language: "En",
       appversion: "ACTIVE",
@@ -90,7 +90,7 @@ describe("/login api where language type is En but DeviceType is wrong", () => {
 });
 
 describe("/login api where language type is English and DeviceType is IOS ", () => {
-  it("user is logged  ", (done) => {
+  it("Logged in Successfully", (done) => {
     const headers = {
       language: "En",
       appversion: "ACTIVE",
@@ -115,7 +115,7 @@ describe("/login api where language type is English and DeviceType is IOS ", () 
 });
 
 describe("/login api where language type is Arab and DeviceType is Ios", () => {
-  it("user is logged  ", (done) => {
+  it("Logged in Successfully", (done) => {
     const headers = {
       language: "Ar",
       appversion: "ACTIVE",
@@ -140,7 +140,7 @@ describe("/login api where language type is Arab and DeviceType is Ios", () => {
 });
 
 describe("/login api where language type is Arab and DeviceType is Ios but osversion is missing", () => {
-  it("user is logged  ", (done) => {
+  it("Logged in Failed", (done) => {
     const headers = {
       language: "Ar",
       appversion: "ACTIVE",
@@ -164,7 +164,7 @@ describe("/login api where language type is Arab and DeviceType is Ios but osver
 });
 
 describe("/login api where language type is Arab and DeviceType is Ios and sending an extra key", () => {
-  it("user is logged  ", (done) => {
+  it("Logged in Successfully", (done) => {
     const headers = {
       language: "Ar",
       appversion: "ACTIVE",
@@ -190,7 +190,7 @@ describe("/login api where language type is Arab and DeviceType is Ios and sendi
 });
 
 describe("/login api where language type is Arab and DeviceType is Ios  and sending deviceid as an integer value instead of a string", () => {
-  it("user is logged  ", (done) => {
+  it("Logged in Failed", (done) => {
     const headers = {
       language: "Ar",
       appversion: "ACTIVE",
@@ -215,13 +215,38 @@ describe("/login api where language type is Arab and DeviceType is Ios  and send
 });
 
 describe("/login api where language type is Arab and DeviceType is Ios  and sending osversion as an integer value instead of a string", () => {
-  it("user is logged  ", (done) => {
+  it("Logged in Successfully", (done) => {
     const headers = {
       language: "Ar",
       appversion: "ACTIVE",
       devicemodel: "galaxy",
       devicetype: "IOS",
       osversion: 6.5
+    };
+    const body = { deviceId: "123" };
+    chai
+      .request("http://localhost:4001")
+      .post("/v1/guest/login")
+      .set(headers)
+      .send(body)
+      .end((err, res) => {
+        if (err) {
+          return "error occured '{err}'";
+        }
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
+
+describe("/login api where language type is Arab and DeviceType is Ios but language is not as defined in constants", () => {
+  it("Logged in Failed", (done) => {
+    const headers = {
+      language: "AR",
+      appversion: "ACTIVE",
+      devicemodel: "galaxy",
+      devicetype: "IOS",
+      osversion : "6.2"
     };
     const body = { deviceId: "123" };
     chai
