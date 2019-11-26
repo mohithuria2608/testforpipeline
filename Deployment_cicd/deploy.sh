ca9b51ddcbdab2a3728778e2313eb8931844d8f1
suruchi <suruchi.singh@appinventiv.com>
2019-11-26 15:52:15 +0530

    Update deploy.sh

--- Deployment_cicd/deploy.sh
+++ Deployment_cicd/deploy.sh
@@ -3,7 +3,7 @@
 
 # read the yml template from a file and substitute the string 
 
-
+pwd
 template=`cat "./deployment.yaml" | sed "s@{{Users_image}}@$Users_image@g"`
 template=`echo "$template" | sed "s@{{Auth_image}}@$Auth_image@g"`
 template=`echo "$template" | sed "s@{{Menu_image}}@$Menu_image@g"`
