584bff68abe5044ffecfb6b1a209dd38fe5872d4
suruchi <suruchi.singh@appinventiv.com>
2019-11-28 10:46:44 +0530

    Update deploy.sh

--- Deployment_cicd/deploy.sh
+++ Deployment_cicd/deploy.sh
@@ -4,7 +4,7 @@
 # read the yml template from a file and substitute the string 
 
 
-template=`cat "./deployment.yaml" | sed "s@{{Users_image}}@$Users_image@g"`
+template=`cat "./Deployment_cicd/deployment.yaml" | sed "s@{{Users_image}}@$Users_image@g"`
 template=`echo "$template" | sed "s@{{Auth_image}}@$Auth_image@g"`
 template=`echo "$template" | sed "s@{{Menu_image}}@$Menu_image@g"`
 template=`echo "$template" | sed "s@{{Order_image}}@$Order_image@g"`
@@ -12,4 +12,4 @@ template=`echo "$template" | sed "s@{{Order_image}}@$Order_image@g"`
 # apply the yml with the substituted value
 echo "$template" | kubectl apply -f Deployment_cicd/Deployment.yaml
 
-kubectl apply -f Deployment_cicd/ingress.yaml
+kubectl apply -f ./Deployment_cicd/ingress.yaml
