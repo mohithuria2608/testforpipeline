0c27d3cc59fac39dda0fa09f57115fce952844ce
Ankit Kumar <ankit.kumar@appinventiv.com>
2020-01-20 04:48:38 +0530

    changes in onboarding flow

--- users-service/src/controllers/v1/user.misc.controller.ts
+++ users-service/src/controllers/v1/user.misc.controller.ts
@@ -16,7 +16,7 @@ export class MiscUserController {
             const tokenType = authObj.id ? Constant.DATABASE.TYPE.TOKEN.USER_AUTH : Constant.DATABASE.TYPE.TOKEN.GUEST_AUTH
             let tokens = await ENTITY.UserE.getTokens(headers.deviceid, headers.devicetype, [tokenType], authObj.id, authObj.isGuest)
             let user = await ENTITY.UserE.getUser({ userId: authObj.id })
-            return { accessToken: tokens.accessToken, response: formatUserData(user, headers.deviceid, headers.country, headers.language) }
+            return { accessToken: tokens.accessToken, response: formatUserData(user, headers) }
         } catch (err) {
             consolelog(process.cwd(), "refreshToken", err, false)
             return Promise.reject(err)
