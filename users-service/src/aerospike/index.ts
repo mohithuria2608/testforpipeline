5c186f1afa96bfee8a170e015c17b9ff6891cf53
Ankit Kumar <ankit.kumar@appinventiv.com>
2020-02-04 12:26:26 +0530

    update in aerospike config

--- users-service/src/aerospike/index.ts
+++ users-service/src/aerospike/index.ts
@@ -51,6 +51,7 @@ class AerospikeClass {
                             scan: defaultPolicy,
                             write: defaultPolicy,
                         },
+                        maxConnsPerNode: 1000
                     }
                     this.client = await aerospike.connect(aerospikeConfig);
                     if (this.client) {
