5ea4f8d9f908650c1004c59f254566f3e86a4cbc
Ankit Kumar <ankit.kumar@appinventiv.com>
2020-02-26 19:37:17 +0530

    language changes

--- order-service/src/route/v1/order.route.ts
+++ order-service/src/route/v1/order.route.ts
@@ -34,6 +34,7 @@ export default (router: Router) => {
                     let headers: ICommonRequest.IHeaders = ctx.request.header;
                     let payload: IOrderRequest.IPostOrder = ctx.request.body;
                     let auth: ICommonRequest.AuthorizationObj = ctx.state.user
+                    console.log("order payload", JSON.stringify(payload))
                     let res = await orderController.postOrder(headers, payload, auth);
                     let sendResponse = sendSuccess(Constant.STATUS_MSG.SUCCESS.S200.DEFAULT, headers.language, res)
                     ctx.status = sendResponse.statusCode;
