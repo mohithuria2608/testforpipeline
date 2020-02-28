d14342f356ef1e0564fbab9ca29df0e8230597d7
Ankit Kumar <ankit.kumar@appinventiv.com>
2020-02-28 12:53:22 +0530

    update bugs

--- order-service/src/route/v1/order.route.ts
+++ order-service/src/route/v1/order.route.ts
@@ -24,7 +24,7 @@ export default (router: Router) => {
                     menuUpdatedAt: Joi.number().error(new Error(Constant.STATUS_MSG.ERROR.E422.DEFAULT_VALIDATION_ERROR.message)),
                     lat: Joi.number().min(0).max(90).error(new Error(Constant.STATUS_MSG.ERROR.E422.INVALID_LOCATION.message)),
                     lng: Joi.number().min(-180).max(180).error(new Error(Constant.STATUS_MSG.ERROR.E422.INVALID_LOCATION.message)),
-                    couponCode: Joi.string().allow("").error(new Error(Constant.STATUS_MSG.ERROR.E422.INVALID_COUPON.message)),
+                    couponCode: Joi.string().lowercase().allow("").error(new Error(Constant.STATUS_MSG.ERROR.E422.INVALID_COUPON.message)),
                     items: Joi.any().error(new Error(Constant.STATUS_MSG.ERROR.E422.INVALID_PRODUCTS.message))
                 }
             }),
