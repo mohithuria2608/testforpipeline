23c2c50b7877a56139d8a10a728c3d6bca89ffeb
Ankit Kumar <ankit.kumar@appinventiv.com>
2020-03-06 20:47:47 +0530

    update order and payment status on cms

--- order-service/src/controllers/v1/order.controller.ts
+++ order-service/src/controllers/v1/order.controller.ts
@@ -263,6 +263,7 @@ export class OrderController {
             return Promise.reject(error)
         }
     }
+
     /**
      * @method GET
      * @param {string=} cCode
@@ -309,6 +310,26 @@ export class OrderController {
 
         } catch (error) {
             consolelog(process.cwd(), "trackOrder", JSON.stringify(error), false)
+            return Promise.reject(error)
+        }
+    }
+
+    async bootstrapPendingOrders() {
+        try {
+            let getPendingOrders = await ENTITY.OrderE.getMultipleMdb({
+                status: Constant.DATABASE.STATUS.ORDER.PENDING.MONGO
+            }, { sdmOrderRef: 1, createdAt: 1, status: 1 }, { lean: true })
+            if (getPendingOrders && getPendingOrders.length > 0) {
+                getPendingOrders.forEach(order => {
+                    if ((order.createdAt + (8 * 60 * 1000)) > new Date().getTime())
+                        ENTITY.OrderE.getSdmOrder({
+                            sdmOrderRef: order.sdmOrderRef,
+                            timeInterval: getFrequency(order.status, Constant.DATABASE.TYPE.FREQ_TYPE.GET_ONCE).nextPingMs
+                        })
+                });
+            }
+        } catch (error) {
+            consolelog(process.cwd(), "bootstrapPendingOrders", JSON.stringify(error), false)
             return Promise.reject(error)
         }
     }
