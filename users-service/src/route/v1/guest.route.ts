e75e110470100c7255cc5328e5cfa37020ce1185
Ankit Kumar <ankit.kumar@appinventiv.com>
2020-03-12 12:49:30 +0530

    update payment bug

--- users-service/src/route/v1/guest.route.ts
+++ users-service/src/route/v1/guest.route.ts
@@ -20,7 +20,7 @@ export default (router: Router) => {
                     let headers: ICommonRequest.IHeaders = ctx.request.header;
                     let payload: IGuestRequest.IGuestLogin = ctx.request.body;
                     let res = await guestController.guestLogin(headers, payload);
-                    ctx.set({ 'accessToken': res.accessToken, 'refreshToken': res.refreshToken })
+                    ctx.set({ 'accessToken': res.accessToken, 'refreshToken': res.refreshToken, cartId: res.response['cartId'] })
                     let sendResponse = sendSuccess(Constant.STATUS_MSG.SUCCESS.S200.LOGIN, headers.language, res.response)
                     ctx.status = sendResponse.statusCode;
                     ctx.body = sendResponse
