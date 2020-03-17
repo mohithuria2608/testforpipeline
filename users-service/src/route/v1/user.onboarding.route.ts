2e82378c512626873e827cd571d0c67c67fa6f86
Ankit Kumar <ankit.kumar@appinventiv.com>
2020-03-17 19:52:44 +0530

    merge with dev

--- users-service/src/route/v1/user.onboarding.route.ts
+++ users-service/src/route/v1/user.onboarding.route.ts
@@ -50,7 +50,13 @@ export default (router: Router) => {
                     let headers: ICommonRequest.IHeaders = ctx.request.header;
                     let payload: IUserRequest.IAuthVerifyOtp = ctx.request.body;
                     let res = await userController.verifyOtp(headers, payload);
-                    ctx.set({ 'accessToken': res.accessToken, 'refreshToken': res.refreshToken, cartId: res.response['cartId'] })
+                    ctx.set({
+                        'accessToken': res.accessToken,
+                        'refreshToken': res.refreshToken,
+                        cartId: res.response['cartId'],
+                        phnNo: res.response['phnNo'],
+                        cCode: res.response['cCode'],
+                    })
                     let sendResponse = sendSuccess(Constant.STATUS_MSG.SUCCESS.S200.OTP_VERIFIED, headers.language, res.response)
                     ctx.status = sendResponse.statusCode;
                     ctx.body = sendResponse
