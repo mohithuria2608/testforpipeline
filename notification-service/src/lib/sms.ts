3ff696b7748d83002c3180df89a4a023f1f0f4f7
Ankit Kumar <ankit.kumar@appinventiv.com>
2020-03-18 14:57:05 +0530

    update in

--- notification-service/src/lib/sms.ts
+++ notification-service/src/lib/sms.ts
@@ -18,41 +18,43 @@ export class SmsCLass {
     constructor() { }
 
     async sendSMS(payload: ISmsRequest.ISingleSms) {
-        return {}
-        // return new Promise((resolve, reject) => {
-        //     payload.message = Helper.utfConverter(payload.message);
-        //     request.get(
-        //         `https://${this.host}/${this.endPoint}?username=${this.userName}&password=${this.password}&type=2&dlr=0&destination=${payload.destination}&source=${this.source}&message=${payload.message}`,
-        //         function (err, data, b) {
-        //             kafkaService.kafkaSync({
-        //                 set: Constant.SET_NAME.LOGGER,
-        //                 mdb: {
-        //                     create: true,
-        //                     argv: JSON.stringify({
-        //                         type: Constant.DATABASE.TYPE.ACTIVITY_LOG.SMS,
-        //                         info: {
-        //                             request: {
-        //                                 body: payload
-        //                             },
-        //                             response: err ? err : b
-        //                         },
-        //                         description: `https://${this.host}/${this.endPoint}?username=${this.userName}&password=${this.password}&type=2&dlr=0&destination=${payload.destination}&source=${this.source}&message=${payload.message}`,
-        //                         options: {
-        //                             env: Constant.SERVER.ENV[config.get("env")],
-        //                         },
-        //                         createdAt: new Date().getTime(),
-        //                     })
-        //                 },
-        //                 inQ: true
-        //             })
-        //             if (err) { reject(err); }
-        //             else {
-        //                 console.log("--> ", b);
-        //                 resolve({ success: true, data: b });
-        //             }
-        //         }
-        //     );
-        // });
+        if (process.env.NODE_ENV == "staging")
+            return {}
+        else
+            return new Promise((resolve, reject) => {
+                payload.message = Helper.utfConverter(payload.message);
+                request.get(
+                    `https://${this.host}/${this.endPoint}?username=${this.userName}&password=${this.password}&type=2&dlr=0&destination=${payload.destination}&source=${this.source}&message=${payload.message}`,
+                    function (err, data, b) {
+                        kafkaService.kafkaSync({
+                            set: Constant.SET_NAME.LOGGER,
+                            mdb: {
+                                create: true,
+                                argv: JSON.stringify({
+                                    type: Constant.DATABASE.TYPE.ACTIVITY_LOG.SMS,
+                                    info: {
+                                        request: {
+                                            body: payload
+                                        },
+                                        response: err ? err : b
+                                    },
+                                    description: `https://${this.host}/${this.endPoint}?username=${this.userName}&password=${this.password}&type=2&dlr=0&destination=${payload.destination}&source=${this.source}&message=${payload.message}`,
+                                    options: {
+                                        env: Constant.SERVER.ENV[config.get("env")],
+                                    },
+                                    createdAt: new Date().getTime(),
+                                })
+                            },
+                            inQ: true
+                        })
+                        if (err) { reject(err); }
+                        else {
+                            console.log("--> ", b);
+                            resolve({ success: true, data: b });
+                        }
+                    }
+                );
+            });
     }
 }
 
