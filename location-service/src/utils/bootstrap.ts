c2f40b204d9369f765a1f5947ee612e8203df9c4
Abhishek Pathak <abhishek.pathak@appinventiv.com>
2020-03-30 10:10:12 +0530

    Updated bootstrap

--- location-service/src/utils/bootstrap.ts
+++ location-service/src/utils/bootstrap.ts
@@ -1,7 +1,6 @@
 import { Aerospike } from "../aerospike";
 import { countryController, cityController, areaController, storeController, locationController } from '../controllers';
 import { configuration } from '../configuration';
-import * as fs from 'fs';
 import * as ENTITY from "../entity";
 
 
@@ -9,10 +8,9 @@ export let bootstrap = async function (server) {
     await Aerospike.init();
     await configuration.init({ bootstrap: true });;
     await countryController.bootstrapCountry()
-    // await cityController.bootstrapCity()
-    // await areaController.bootstrapArea()
-    // await storeController.bootstrapStore()
-    // locationController.bootstrapPickup()
-    fs.writeFileSync('storesList.json', JSON.stringify(await ENTITY.StoreE.getAllStores()));
+    await cityController.bootstrapCity()
+    await areaController.bootstrapArea()
+    await storeController.bootstrapStore()
+    locationController.bootstrapPickup()
     return {}
 }
