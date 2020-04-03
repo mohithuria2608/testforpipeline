646500f310e26a8a4a1563b41ed0b59625732408
Ankit Kumar <ankit.kumar@appinventiv.com>
2020-04-03 14:46:35 +0530

    update

--- order-service/src/controllers/v1/order.controller.ts
+++ order-service/src/controllers/v1/order.controller.ts
@@ -272,7 +272,7 @@ export class OrderController {
             else
                 return Promise.reject(Constant.STATUS_MSG.ERROR.E409.ORDER_NOT_FOUND)
             console.log("..............aaaaa", sdmOrder)
-            if (sdmOrder === NaN)
+            if (sdmOrder == NaN)
                 return Promise.reject(Constant.STATUS_MSG.ERROR.E422.INVALID_ORDER)
             let userData: IUserRequest.IUserData
             if (payload.cCode && payload.phnNo) {
