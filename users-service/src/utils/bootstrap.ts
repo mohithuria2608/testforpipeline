e1a6a4e31325497ae96d3de258003ecc19fdae38
Ankit Kumar <ankit.kumar@appinventiv.com>
2020-04-05 20:23:57 +0530

    update

--- users-service/src/utils/bootstrap.ts
+++ users-service/src/utils/bootstrap.ts
@@ -2,13 +2,11 @@ import { Aerospike } from "../aerospike";
 import { SDM } from '../sdm'
 import { configuration } from '../sync/configuration';
 import { appversion } from '../sync/appversion';
-import { faq } from '../sync/faq';
 
 export let bootstrap = async function (server) {
     await Aerospike.init();
     await configuration.init({ bootstrap: true });
     await appversion.init();
-    await faq.init();
     await SDM.init();
     return {}
 }
