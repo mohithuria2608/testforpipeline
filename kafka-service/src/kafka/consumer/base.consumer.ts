d895587d9d414069223fd7f099c6461d6f2fb85b
Ankit Kumar <ankit.kumar@appinventiv.com>
2020-04-15 04:22:10 +0530

    update in payment config

--- kafka-service/src/kafka/consumer/base.consumer.ts
+++ kafka-service/src/kafka/consumer/base.consumer.ts
@@ -42,7 +42,7 @@ export class BaseConsumer {
                 consumerData
             ],
             {
-                autoCommit: true,
+                autoCommit: false,
 
             }
         );
