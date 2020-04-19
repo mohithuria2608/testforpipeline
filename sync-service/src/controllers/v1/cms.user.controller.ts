7b3462ddc7104a69391e246f15163149ca3d7c71
Ankit Kumar <ankit.kumar@appinventiv.com>
2020-04-20 02:13:38 +0530

    file read from local

--- sync-service/src/controllers/v1/cms.user.controller.ts
+++ sync-service/src/controllers/v1/cms.user.controller.ts
@@ -1,23 +1,25 @@
 import { consolelog } from '../../utils'
 import * as ENTITY from '../../entity'
 import * as request from 'request-promise';
-
+const fs = require('fs')
 export class CmsUserController {
 
     constructor() { }
 
     async migrateUsersFromBlob(payload: any) {
         try {
+
             let counter = 0
             for (let i = payload.start; i <= payload.end; i++) {
                 let timeout = counter * 20000
                 console.log("i", timeout, i)
                 setTimeout(async () => {
-                    let url = `https://kfcprodnecmsimage.blob.core.windows.net/americana/exports/user_${i}.json`
-                    let users = await request.get(url, {});
+                    // let url = `https://kfcprodnecmsimage.blob.core.windows.net/americana/exports/user_${i}.json`
+                    // let users = await request.get(url, {});
+                    let users = JSON.parse(fs.readFileSync(`${process.cwd()}/src/user_${i}.json`, 'utf-8'))
                     if (users) {
-                        console.log(JSON.parse(users).length)
-                        ENTITY.UserE.postUser(JSON.parse(users));
+                        console.log(users.length)
+                        ENTITY.UserE.postUser(users);
                     }
                 }, timeout)
                 counter = counter + 1
