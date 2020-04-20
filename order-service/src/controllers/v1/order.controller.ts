fdfbc5cd98271040558a44cc94c79d38bcb15a09
Ankit Kumar <ankit.kumar@appinventiv.com>
2020-04-20 18:42:52 +0530

    update in configs

--- order-service/src/controllers/v1/order.controller.ts
+++ order-service/src/controllers/v1/order.controller.ts
@@ -133,9 +133,9 @@ export class OrderController {
 
             let totalAmount = cart.amount.filter(obj => { return obj.type == Constant.DATABASE.TYPE.CART_AMOUNT.TYPE.TOTAL })
             if (totalAmount[0].amount < Constant.CONF.COUNTRY_SPECIFIC[headers.country].MIN_CART_VALUE)
-                return Promise.reject(Constant.STATUS_MSG.ERROR.E400.MIN_CART_VALUE_VOILATION)
+                return Promise.reject(Constant.STATUS_MSG.ERROR.E400.MIN_CART_VALUE_VOILATION(Constant.CONF.COUNTRY_SPECIFIC[headers.country].MIN_CART_VALUE))
             if (totalAmount[0].amount > Constant.CONF.COUNTRY_SPECIFIC[headers.country].MIN_COD_CART_VALUE && payload.paymentMethodId == Constant.DATABASE.TYPE.PAYMENT_METHOD_ID.COD)
-                return Promise.reject(Constant.STATUS_MSG.ERROR.E400.MAX_COD_CART_VALUE_VOILATION)
+                return Promise.reject(Constant.STATUS_MSG.ERROR.E400.MAX_COD_CART_VALUE_VOILATION(Constant.CONF.COUNTRY_SPECIFIC[headers.country].MIN_COD_CART_VALUE))
 
             let order: IOrderRequest.IOrderData = await ENTITY.OrderE.createOrderMongo(headers, payload, cart, getAddress, store, userData)
             consolelog(process.cwd(), "step 6", new Date(), false)
